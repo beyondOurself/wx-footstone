49a6be144669dbb4bf072ca4bf47140bd9cf48dc
15099836861 <562172151@qq.com>
2020-04-10 17:56:13 +0800

    新增 > tabbar

---
+++ custom-tab-bar/index.js
@@ -0,0 +1,47 @@
+Component({
+  data: {
+    selected: 0,
+    color: "#7A7E83",
+    selectedColor: "#3cc51f",
+    list: [{
+      pagePath: "/pages/index/home",
+      iconPath: "/image/icon_component.png",
+      selectedIconPath: "/image/icon_component_HL.png",
+      text: "首页"
+    }
+    , 
+    {
+      pagePath: "/pages/index/toread",
+      iconPath: "/image/icon_API.png",
+      selectedIconPath: "/image/icon_API_HL.png",
+      text: "待阅"
+    }
+    , 
+    {
+      pagePath: "/pages/index/backlog",
+      iconPath: "/image/icon_API.png",
+      selectedIconPath: "/image/icon_API_HL.png",
+      text: "待办"
+    }
+    , 
+    {
+      pagePath: "/pages/index/me",
+      iconPath: "/image/icon_API.png",
+      selectedIconPath: "/image/icon_API_HL.png",
+      text: "我的"
+    }
+  ]
+  },
+  attached() {
+  },
+  methods: {
+    switchTab(e) {
+      const data = e.currentTarget.dataset
+      const url = data.path
+      wx.switchTab({url})
+      this.setData({
+        selected: data.index
+      })
+    }
+  }
+})
