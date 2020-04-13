553710ec9ce3607f03dd33d8078953dc48550ede
15099836861 <562172151@qq.com>
2020-04-13 15:13:25 +0800

    新增 用户登录过后就不用在登录了

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const  $api =  require("./services/index")
 App({
   onLaunch: function () {
     //如果已经存储了用户的信息就不用在登录了
-    if (false) {
+
+    if (wx.getStorageSync('userInfo')) {
       wx.switchTab({
         url: 'pages/index/home',
       })
