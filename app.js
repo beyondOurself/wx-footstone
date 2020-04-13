39e76febc795707c0ec395a29fc2e44419e85447
15099836861 <562172151@qq.com>
2020-04-13 17:44:42 +0800

    trifles

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const  $api =  require("./services/index")
 App({
   onLaunch: function () {
     //如果已经存储了用户的信息就不用在登录了
-
     if (wx.getStorageSync('userInfo')) {
       wx.switchTab({
         url: 'pages/index/home',
