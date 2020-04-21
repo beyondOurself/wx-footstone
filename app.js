c1da40568af4d12bea27b65683f646d04a3f76c6
15099836861 <562172151@qq.com>
2020-04-21 09:00:37 +0800

    放开 > app.js : 登录时候自动跳转.

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ App({
   
 
   onLaunch: function () {  
-
-    //如果已经存储了用户的信息就不用在登录了
-    // if (wx.getStorageSync('userInfo')) {
-    //   wx.switchTab({
-    //     url: 'pages/index/home',
-    //   })
-    // }
+    如果已经存储了用户的信息就不用在登录了
+    if (wx.getStorageSync('userInfo')) {
+      wx.switchTab({
+        url: 'pages/index/home',
+      })
+    }
   }, 
   onShow:function(){
      
